db8d329e8a44265d5942aee16b8afef30432854c
shashwat558 <jainshashwat528@gmail.com>
2024-11-07 23:37:45 +0530

    Done some Componentization and fixed the backend route

---
+++ app/creator/[creatorId]/page.tsx
@@ -0,0 +1,11 @@
+import StreamView from "@/app/components/StreamView"
+
+export default function create(
+    {
+        params: {creatorId}
+    }: {params:{
+        creatorId: string
+    }}
+){
+    return <StreamView creatorId={creatorId}/>
+}
